da1f34488ec9e954458290dcba1aab0e86b2b713
isaabellepontes <isaapontes010@gmail.com>
2023-07-10 17:20:57 -0300

    comentando o js

--- js/script.js
+++ js/script.js
@@ -2,23 +2,23 @@ window.onload = function() {
     var categoriaSelect = document.getElementById('categoria');
     categoriaSelect.addEventListener('change', atualizarUnidades);
     atualizarCategorias();
-  
+    
     var converterButton = document.querySelector('button');
     converterButton.addEventListener('click', converter);
   };
-  
+  //Função para atualizar as categorias.
   function atualizarCategorias() {
     var categoriaSelect = document.getElementById('categoria');
     categoriaSelect.innerHTML = '';
-  
+  // Colocando as categorias disponiveis
     var categorias = ['comprimento', 'peso', 'temperatura'];
-  
+    
     categorias.forEach(function(categoria) {
       var option = document.createElement('option');
       option.textContent = categoria;
       categoriaSelect.appendChild(option);
     });
-  
+  //Executa a função para atualizar as unidades
     atualizarUnidades();
   }
   
@@ -27,14 +27,14 @@ window.onload = function() {
     var unidadeOrigemSelect = document.getElementById('Origem');
     var unidadeDestinoSelect = document.getElementById('Destino');
     var categoriaSelecionada = categoriaSelect.value;
-  
+    
     // Limpar as opções existentes
     unidadeOrigemSelect.innerHTML = '';
     unidadeDestinoSelect.innerHTML = '';
-  
+  // Define as unidades de origem e destino de acordo com a categoria selecionada
     var unidadesOrigem = [];
     var unidadesDestino = [];
-  
+    
     switch (categoriaSelecionada) {
       case 'comprimento':
         unidadesOrigem = ['metros', 'centimetros', 'polegadas'];
@@ -51,34 +51,34 @@ window.onload = function() {
       default:
         break;
     }
-  
+  // Para as unidades de Origem, cria as opçoes e adiciona no campo Origem
     unidadesOrigem.forEach(function(unidade) {
       var option = document.createElement('option');
       option.textContent = unidade;
       unidadeOrigemSelect.appendChild(option);
     });
-  
+  // Para as unidades de Destino, cria as opçoes e adiciona no campo Destino
     unidadesDestino.forEach(function(unidade) {
       var option = document.createElement('option');
       option.textContent = unidade;
       unidadeDestinoSelect.appendChild(option);
     });
   }
-  
+  // Função para fazer a conversão
   function converter() {
     var valorInput = document.getElementById('valor');
     var unidadeOrigemSelect = document.getElementById('Origem');
     var unidadeDestinoSelect = document.getElementById('Destino');
     var resultadoOutput = document.getElementById('resultado');
-  
+  // Obtem o valor numerico para a conversão
     var valor = parseFloat(valorInput.value);
+  //Obtem as unidades de origem e destino.
     var Origem = unidadeOrigemSelect.value;
     var Destino = unidadeDestinoSelect.value;
-  
     // conversão por Categoria
     var categoriaSelecionada = document.getElementById('categoria').value;
     var resultado;
-  
+  // Faz a conversão de acordo com a categoria
     switch (categoriaSelecionada) {
       case 'comprimento':
         resultado = converterComprimento(valor, Origem, Destino);
@@ -92,7 +92,7 @@ window.onload = function() {
       default:
         break;
     }
-  
+  //Texto mais resultado
     resultadoOutput.textContent = valor + ' ' + Origem + ' e igual a ' + resultado + ' ' + Destino;
   }
   
@@ -111,7 +111,6 @@ window.onload = function() {
       default:
         break;
     }
-  
     // Conversão da unidade de origem para a unidade de destino
     switch (Destino) {
       case 'metros':
@@ -124,7 +123,6 @@ window.onload = function() {
         break;
     }
   }
-  
   function converterPeso(valor, Origem, Destino) {
     // Conversão para quilogramas
     switch (Origem) {
@@ -140,7 +138,6 @@ window.onload = function() {
       default:
         break;
     }
-  
     // Conversão da unidade de origem para a unidade de destino
     switch (Destino) {
       case 'quilogramas':
@@ -169,7 +166,6 @@ window.onload = function() {
       default:
         break;
     }
-  
     // Conversão da unidade de origem para a unidade de destino
     switch (Destino) {
       case 'Celsius':
@@ -182,6 +178,3 @@ window.onload = function() {
         break;
     }
   }
-  
-  
-  
